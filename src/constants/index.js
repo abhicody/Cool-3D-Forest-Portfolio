2902995645214bf4acb2ff7e4935d78d8d7afb3b
abhicody <honeydude009@gmail.com>
2024-10-09 13:29:37 +0530

    linked in and github links in footer

--- src/constants/index.js
+++ src/constants/index.js
@@ -235,12 +235,12 @@ export const socialLinks = [
   {
     name: "GitHub",
     iconUrl: github,
-    link: "https://github.com/YourGitHubUsername",
+    link: "https://github.com/abhicody",
   },
   {
     name: "LinkedIn",
     iconUrl: linkedin,
-    link: "https://www.linkedin.com/in/YourLinkedInUsername",
+    link: "https://www.linkedin.com/in/abhisekh-singh-b808b35a/",
   },
 ];
 
